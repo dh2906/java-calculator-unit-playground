0b0bd61f4df7e15cc63afd638b51585a7eb9309a
Donghun <2dh2@naver.com>
2025-07-30 23:20:04 +0900

    feat. 사칙연산 예외 발생 단위 테스트 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -41,4 +41,36 @@ public class CalculatorTest {
 
         assertEquals(num1 / num2, actual);
     }
+
+    @Test
+    public void 덧셈_오버플로우_예외_발생() {
+        final int num1 = Integer.MAX_VALUE;
+        final int num2 = Integer.MAX_VALUE;
+
+        assertThrows(ArithmeticException.class, () -> calculator.add(num1, num2));
+    }
+
+    @Test
+    public void 뺄셈_오버플로우_예외_발생() {
+        final int num1 = Integer.MIN_VALUE;
+        final int num2 = Integer.MAX_VALUE;
+
+        assertThrows(ArithmeticException.class, () -> calculator.sub(num1, num2));
+    }
+
+    @Test
+    public void 곱셈_오버플로우_예외_발생() {
+        final int num1 = Integer.MAX_VALUE;
+        final int num2 = Integer.MAX_VALUE;
+
+        assertThrows(ArithmeticException.class, () -> calculator.mul(num1, num2));
+    }
+
+    @Test
+    public void 나눗셈_0으로_나누면_예외_발생() {
+        final int num1 = Integer.MAX_VALUE;
+        final int num2 = 0;
+
+        assertThrows(ArithmeticException.class, () -> calculator.div(num1, num2));
+    }
 }
