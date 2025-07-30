faa2f887a6c9f57e952448cfae27c1da4a59dafe
Donghun <2dh2@naver.com>
2025-07-30 23:00:48 +0900

    feat. 누락된 메소드 접근 제어자 추가

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -1,19 +1,19 @@
 package domain;
 
 public class Calculator {
-    int add(int num1, int num2) {
+    public int add(int num1, int num2) {
         return Math.addExact(num1, num2);
     }
 
-    int sub(int num1, int num2) {
+    public int sub(int num1, int num2) {
         return Math.subtractExact(num1, num2);
     }
 
-    int mul(int num1, int num2) {
+    public int mul(int num1, int num2) {
         return Math.multiplyExact(num1, num2);
     }
 
-    int div(int num1, int num2) {
+    public int div(int num1, int num2) {
         if (num2 == 0)
             throw new RuntimeException("0으로 나눌 수 없습니다.");
 
