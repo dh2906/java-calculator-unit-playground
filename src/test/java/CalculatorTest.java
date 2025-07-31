6aaeab58aa846749b0c627306514c1b45449cdac
Donghun <2dh2@naver.com>
2025-07-31 18:18:54 +0900

    feat. 사칙연산 단위 테스트 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,66 +1,44 @@
-import org.junit.jupiter.api.DisplayName;
+import domain.Calculator;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("계산기 기능 테스트 클래스")
 public class CalculatorTest {
-    @Test
-    @DisplayName("더하기 연산 테스트")
-    public void testAdd() {
-        int num1 = 1, num2 = 2;
+    private final Calculator calculator = new Calculator();
 
-        assertThat(3).isEqualTo(Calculator.add(num1, num2));
+    @Test
+    public void 덧셈_테스트() {
+        final int num1 = 1;
+        final int num2 = 2;
+        final int actual = calculator.add(num1, num2);
+
+        assertEquals(num1 + num2, actual);
     }
 
     @Test
-    @DisplayName("빼기 연산 테스트")
-    public void testSub() {
-        int num1 = 1, num2 = 2;
+    public void 뺄셈_테스트() {
+        final int num1 = 3;
+        final int num2 = 2;
+        final int actual = calculator.sub(num1, num2);
 
-        assertThat(-1).isEqualTo(Calculator.sub(num1, num2));
+        assertEquals(num1 - num2, actual);
     }
 
     @Test
-    @DisplayName("곱하기 연산 테스트")
-    public void testMul() {
-        int num1 = 10, num2 = 2;
+    public void 곱셈_테스트() {
+        final int num1 = 3;
+        final int num2 = 2;
+        final int actual = calculator.mul(num1, num2);
 
-        assertThat(20).isEqualTo(Calculator.mul(num1, num2));
+        assertEquals(num1 * num2, actual);
     }
 
     @Test
-    @DisplayName("나누기 연산 테스트")
-    public void testDiv() {
-        int num1 = 1, num2 = 2;
+    public void 나눗셈_테스트() {
+        final int num1 = 4;
+        final int num2 = 2;
+        final int actual = calculator.div(num1, num2);
 
-        assertThat(0).isEqualTo(Calculator.div(num1, num2));
-    }
-
-    @Test
-    @DisplayName("나누기 연산에서 나누는 값이 0인지 테스트")
-    public void testDivideByZero() {
-        int num1 = 2, num2 = 0;
-
-        assertThatThrownBy(() -> {
-            Calculator.div(num1, num2);
-        }).isInstanceOf(ArithmeticException.class);
-    }
-
-    @Test
-    @DisplayName("연산 후 int 범위를 벗어나는 지 테스트")
-    public void testOverflow() {
-        assertThatThrownBy(() -> {
-            Calculator.add(Integer.MAX_VALUE, 1);
-        }).isInstanceOf(ArithmeticException.class);
-
-        assertThatThrownBy(() -> {
-            Calculator.sub(Integer.MIN_VALUE, 1);
-        }).isInstanceOf(ArithmeticException.class);
-
-        assertThatThrownBy(() -> {
-            Calculator.mul(Integer.MAX_VALUE, 2);
-        }).isInstanceOf(ArithmeticException.class);
+        assertEquals(num1 / num2, actual);
     }
 }
