b2a779c44de146f7e93ac99aceaf226a94e8c57b
Donghun <2dh2@naver.com>
2025-08-02 16:30:36 +0900

    refactor. 문자열 유틸 클래스로 분할

--- src/main/java/domain/StringCalculator.java
+++ src/main/java/domain/StringCalculator.java
@@ -1,6 +1,8 @@
 package domain;
 
 import exception.ErrorMessage;
+import util.DelimiterUtil;
+import util.ParseUtil;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,30 +18,15 @@ public class StringCalculator {
         if (str.isBlank())
             return 0;
 
-        String customDelimiter = findCustomDelimiter(str);
+        String customDelimiter = DelimiterUtil.findCustomDelimiter(str);
         String strNumbers = extractNumber(str, customDelimiter);
         String[] tokens = splitTokens(strNumbers, customDelimiter);
-        List<Integer> numbers = parseNumber(tokens);
+        List<Integer> numbers = ParseUtil.parseNumber(tokens);
 
         return sum(numbers);
     }
 
-    public String findCustomDelimiter(String str) {
-        int startDelimiterIdx = str.indexOf("//");
-        int endDelimiterIdx = str.indexOf("\n");
-
-        if (startDelimiterIdx == -1 && endDelimiterIdx == -1) {
-            return null;
-        }
-
-        if ((startDelimiterIdx == -1) ^ (endDelimiterIdx == -1) || (startDelimiterIdx + 2 == endDelimiterIdx)) {
-            throw new RuntimeException(ErrorMessage.CUSTOM_DELIMITER_NOT_FOUND);
-        }
-
-        return str.substring(startDelimiterIdx + 2, endDelimiterIdx);
-    }
-
-    public String extractNumber(String str, String customDelimiter) {
+    private String extractNumber(String str, String customDelimiter) {
         if (customDelimiter != null) {
             return str.substring(str.indexOf("\n") + 1);
         }
@@ -47,7 +34,7 @@ public class StringCalculator {
         return str;
     }
 
-    public String[] splitTokens(String strNumbers, String customDelimiter) {
+    private String[] splitTokens(String strNumbers, String customDelimiter) {
         String delimiterRegex = "[,|:]";
 
         if (customDelimiter == null) {
@@ -57,27 +44,7 @@ public class StringCalculator {
         return strNumbers.split(Pattern.quote(customDelimiter));
     }
 
-    public List<Integer> parseNumber(String[] tokens) {
-        return Arrays.stream(tokens)
-                .map(this::parseTokenToInt)
-                .toList();
-    }
-
-    public int parseTokenToInt(String token) {
-        try {
-            int number = Integer.parseInt(token.trim());
-
-            if (number < 0) {
-                throw new RuntimeException(ErrorMessage.NEGATIVE_NUMBER_NOT_ALLOWED);
-            }
-
-            return number;
-        } catch (NumberFormatException ex) {
-            throw new RuntimeException(ErrorMessage.INVALID_STRING);
-        }
-    }
-
-    public int sum(List<Integer> numbers) {
+    private int sum(List<Integer> numbers) {
         return numbers.stream()
                 .mapToInt(Integer::intValue)
                 .sum();
